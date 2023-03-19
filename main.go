b22987ae8397f90dff3e19d4bebc5cbb682169c1
Ann <a.kuchebo@mail.ru>
2023-03-19 13:00:22 +0300

    only code for server push

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+var router *gin.Engine
+
+func main() {
+	fmt.Println(cfg.ServerHost + ":" + cfg.ServerPort)
+
+	router = gin.Default()
+
+	router.LoadHTMLFiles(cfg.HTML + "index.html")
+
+	router.Static("/images", "./images")
+
+	router.GET("/", index)
+
+	router.GET("/images", index)
+
+	router.Run(cfg.ServerHost + ":" + cfg.ServerPort)
+}
+
+func index(c *gin.Context) {
+	c.HTML(200, "index.html", nil)
+}
---
+++ settings.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type settings struct {
+	ServerHost string
+	ServerPort string
+	HTML       string
+	Images     string
+}
+
+var cfg settings
+
+func init() {
+	//try read config
+	file, err := os.Open("settings.cfg")
+	if err != nil {
+		fmt.Println(err.Error())
+
+		panic("Can't open the configuration")
+	}
+	defer file.Close()
+
+	stat, err := file.Stat()
+	if err != nil {
+		fmt.Println(err.Error())
+
+		panic("Cant't read the structure of the configuration")
+	}
+
+	readByte := make([]byte, stat.Size())
+
+	_, err = file.Read(readByte)
+	if err != nil {
+		fmt.Println(err.Error())
+
+		panic("Can't read the configuration")
+	}
+	err = json.Unmarshal(readByte, &cfg)
+	if err != nil {
+		fmt.Println(err.Error())
+
+		panic("Can't read configuratiion data")
+	}
+}
