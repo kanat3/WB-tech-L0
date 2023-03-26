3059e3184fa41a14d5a19e3f50a0372e94d66ebe
Anna <a.kuchebo@mail.ru>
2023-03-27 00:20:36 +0300

    added geting data from bd to page

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	//"Work/WB-tech-L0/datacache"
 
+	"WB-tech-L0/orders"
 	"WB-tech-L0/publisher"
 	"WB-tech-L0/subscriber"
 	"fmt"
@@ -17,58 +18,24 @@ var router *gin.Engine
 func main() {
 
 	var sub *subscriber.Subscriber
-
 	// intitialization
 	sub = subscriber.New()
 
 	// connect to bd
 	db, error := sub.DbConnect()
-
 	if error != nil {
 		fmt.Printf(error.Error())
 		return
 	}
 
 	// get all database to cache
-
-	// pull every note from database to subscriber.cache
 	error = sub.DbToCache(db)
 
 	if error != nil {
 		fmt.Printf(error.Error())
 		return
 	}
-
 	go sub.Run()
-
-	// create data to push
-	/*
-		var order orders.OrderJSON
-
-		order.New("orders/model.json")
-
-		sub.PushOrder(order, db)
-		fmt.Printf("Data id generated: \n %v \n", order.Order_uid)
-	*/
-	// here using get from cache. Ok
-	/*
-		var order_get orders.OrderJSON
-
-		order_get, error = sub.Cache.GetById(order.Order_uid)
-
-		if error != nil {
-			fmt.Printf(error.Error())
-			return
-		}
-
-		fmt.Printf("Data by getted by subcriber: \n %v", order_get)
-	*/
-	// is working
-	//fmt.Printf("Sublisher all data: \n %v \n %v \n %v \n", sub.SubSettings, sub.DbSettings, sub.Cache)
-
-	// try to push it in database
-
-	//data := datacache.New()
 
 	router = gin.Default()
 
@@ -76,23 +43,28 @@ func main() {
 	router.Static("/images", "./images")
 	router.Static("static/css", "./static/css")
 
-	router.GET("/bye_page", bye)
-
 	var pub publisher.Publisher
-	id := ""
 	pub.New()
 
 	go pub.Run()
 
+	var o orders.OrderJSON
 	router.GET("/", func(c *gin.Context) {
 		index(c)
-		id = getId(c)
-		_, error := sub.Cache.GetById(id)
+		id := getId(c)
+		o, error = sub.Cache.GetById(id)
 		if error != nil {
 			fmt.Print(error.Error())
 		} else {
 			fmt.Printf("OK: found order with id: %v\n", id)
 		}
+	})
+
+	router.GET("/bye_page", func(c *gin.Context) {
+		c.PureJSON(200, gin.H{
+			"id":   o.Order_uid,
+			"data": o.DataJSON,
+		})
 	})
 
 	router.GET("/images", images)
@@ -114,10 +86,6 @@ func getId(c *gin.Context) (id string) {
 		return ""
 	}
 	return id
-}
-
-func bye(c *gin.Context) {
-	c.HTML(200, "bye_page.html", nil)
 }
 
 func images(c *gin.Context) {
